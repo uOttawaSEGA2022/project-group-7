3294764ad0e15e7bada6ccba99b56a6381d895a0
Shezaad-Jiffry <sjiff073@uottawa.ca>
2022-10-16 23:25:18 -0400

    firebase integration

--- app/src/main/java/com/example/group7mealerapp/MainActivity.java
+++ app/src/main/java/com/example/group7mealerapp/MainActivity.java
@@ -7,12 +7,20 @@ import android.view.View;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     public void btnRegisterClick(View view)
