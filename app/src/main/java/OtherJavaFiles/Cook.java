ada93630b16bbb92c2817edcc4caef2ee56c7e33
Chujian Xiao <mattcxiao@gmail.com>
2022-10-17 20:55:32 -0400

    Added description attribute to Cook

--- app/src/main/java/OtherJavaFiles/Cook.java
+++ app/src/main/java/OtherJavaFiles/Cook.java
@@ -4,10 +4,12 @@ import android.graphics.Bitmap;
 public class Cook extends User{
     //private String firstName, lastName, email, password;
     private Bitmap blankCheque;
+    private String description;
 
-    public Cook(String firstName,String lastName,String email,String password,String address){
+    public Cook(String firstName,String lastName,String email,String password,String address, String description){
         super(firstName,lastName,email,password,address);
         this.blankCheque = null;
+        this.description = description;
     }
 
 
@@ -17,6 +19,18 @@ public class Cook extends User{
 
     public void setBlankCheque(Bitmap blankCheque) {
         this.blankCheque = blankCheque;
+    }
+
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        if(input == null || input.isEmpty()){
+            throw new IllegalArgumentException("password is invalid");
+        }
+        this.description = description;
     }
 
 }
